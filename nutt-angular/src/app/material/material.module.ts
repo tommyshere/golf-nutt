7dff0d9d96b03e0a2f57945225cb5d88c93d481a
tommyshere <127OTIQaSz%e>
2022-06-07 23:54:24 -0700

    login component

--- nutt-angular/src/app/material/material.module.ts
+++ nutt-angular/src/app/material/material.module.ts
@@ -7,6 +7,8 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [
@@ -18,6 +20,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatDialogModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
   ],
   exports: [
     MatToolbarModule,
@@ -28,6 +32,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatDialogModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
   ],
 })
 export class MaterialModule {}
--- nutt-angular/src/app/views/user/login/login.component.ts
+++ nutt-angular/src/app/views/user/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
