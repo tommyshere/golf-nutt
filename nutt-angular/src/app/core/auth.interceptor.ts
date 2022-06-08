26189042da05fe97229eec5c2c8fd091a2509fc3
tommyshere <127OTIQaSz%e>
2022-06-07 22:32:08 -0700

    http service interceptor

--- nutt-angular/src/app/core/auth.interceptor.ts
+++ nutt-angular/src/app/core/auth.interceptor.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
-  HttpEvent,
   HttpInterceptor,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
-  intercept(
-    request: HttpRequest<unknown>,
-    next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    const authToken = this.authService.getToken();
+    request = request.clone({
+      setHeaders: {
+        Authorization: 'Bearer ' + authToken,
+      },
+    });
     return next.handle(request);
   }
 }
